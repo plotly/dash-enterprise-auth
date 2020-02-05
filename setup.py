5bd5cffa2fdf70460f5dc986696c5092bbd7ed0d
Antoine Roy-Gobeil <antoine@plot.ly>
2020-02-05 18:18:31 -0500

    setup.py: add python_requires

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "requests[security]",
         "retrying"
     ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
